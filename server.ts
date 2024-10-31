e5decfcd4fe9f75247e679b2c25ce319bde8a645
Pablo Merino <pablonicolasm@hotmail.com>
2024-10-31 15:05:19 -0300

    cors fixed

--- server.ts
+++ server.ts
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: `http://${HOST}:${PORT}`,
+    origin: `http://${HOST}:8000`,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
