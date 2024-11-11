e8feac658b78097299866a19a131c097484cbb0e
PabloNMerino <pablonicolasm@hotmail.com>
2024-11-11 16:39:23 -0300

    ticket generation done

--- ticket/service/ticketService.ts
+++ ticket/service/ticketService.ts
@@ -3,20 +3,29 @@ import QRCode from 'qrcode';
 
 class TicketService {
 
-    async createNewTicket(eventId: String, buyerId: String, quantity: Number) {
+    async createNewTicket(eventId: string, buyerId: string, quantity: number): Promise<void> {
         try {
-            const ticket = new Ticket({
-                eventId,
-                buyerId
+            // Crear un array de promesas para crear todos los tickets
+            const ticketPromises = Array.from({ length: quantity }, async () => {
+                // Crear un nuevo ticket sin guardar aún en la base de datos
+                const ticket = new Ticket({
+                    eventId,
+                    buyerId
+                });
+    
+                // Generar el código QR
+                const qrCodeData = `http://${process.env.HOST}:${process.env.PORT}/tickets/${ticket._id}`;
+                const qrCode = await QRCode.toDataURL(qrCodeData);
+    
+                // Asignar el código QR al ticket
+                ticket.qrCode = qrCode;
+    
+                // Guardar el ticket en la base de datos
+                return Ticket.create(ticket);
             });
-
-            await Ticket.create(ticket);
-
-            const qrCodeData = `http://${process.env.HOST}:${process.env.PORT}/tickets/${ticket._id}`;
-            const qrCode = await QRCode.toDataURL(qrCodeData);
-
-            ticket.qrCode = qrCode;
-            ticket.save();
+    
+            // Ejecutar todas las promesas para crear los tickets
+            await Promise.all(ticketPromises);
         } catch (error) {
             throw error;
         }
@@ -24,3 +33,4 @@ class TicketService {
 }
 
 export const ticketService = new TicketService();
+
