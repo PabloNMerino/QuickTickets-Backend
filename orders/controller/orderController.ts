b4988ed49fcb180e6b315ae7b722f14db3639c90
PabloNMerino <pablonicolasm@hotmail.com>
2024-11-18 20:04:17 -0300

    scheduler implementado

--- orders/controller/orderController.ts
+++ orders/controller/orderController.ts
@@ -5,37 +5,55 @@ import { ticketService } from "../../ticket/service/ticketService"
 import Event from "../../events/model/eventModel"
 import User from "../../users/model/userModel"
 import { emailService } from "../../email/service/emailService"
+import schedule from "node-schedule"
 
 class OrderController {
 
     async newOrder(req: Request, res: Response) {
         const eventId = req.body.eventId as string;
         const quantity = parseInt(req.body.quantity as string, 10);
-        
+    
         try {
             const userId = req.userId;
             if (!userId) {
                 return res.status(400).json({ error: 'User ID is required' });
             }
+    
             const event = await Event.findById(eventId);
-            const user = await User.findById(userId, 'email')
+            const user = await User.findById(userId, 'email');
             const newOrder = Order.create({
                 eventId,
                 quantity,
-                buyerId: userId
+                buyerId: userId,
             });
-
+    
             await eventService.discountAvailabilityAmount(eventId, quantity);
             await ticketService.createNewTicket(eventId, userId, quantity);
-            
-            if(user!=null && event!=null) {
-                await emailService.sendPurchaseEmail(user.email, event.name, event.dateTime, quantity)
+    
+            if (user != null && event != null) {
+                await emailService.sendPurchaseEmail(user.email, event.name, event.dateTime, quantity);
+    
+                const eventDate = event?.dateTime;
+                if (!eventDate) {
+                    return res.status(400).json({ error: 'Event date is missing' });
+                }
+    
+                const eventDateModified = new Date(eventDate);
+                eventDateModified.setDate(eventDateModified.getDate() - 7);
+                console.log(eventDateModified);
+    
+                // Programar el recordatorio
+                schedule.scheduleJob(eventDateModified, function () {
+                    emailService.sendReminderEmail(user.email, event.name, event.dateTime, quantity);
+                });
             }
+    
             res.status(200).json(newOrder);
         } catch (error) {
             res.status(400).json({ error });
         }
     }
+
 }
 
 export const orderController = new OrderController();
