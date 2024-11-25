8025bd4b2322a1f6412aad534f83c7552c359a54
PabloNMerino <pablonicolasm@hotmail.com>
2024-11-25 18:29:12 -0300

    verifier role added

--- middlewares/index.ts
+++ middlewares/index.ts
@@ -42,3 +42,20 @@ export function isAuthenticated(req: Request, res: Response, next: Function) {
         return res.status(401).json({message:"Unauthorized: Invalid token"});
     }
 }
+
+export function isVerifier(req: Request, res: Response, next: Function) {
+    const token = req.headers;
+    try {
+        const isTokenvalid: any = jwt.verify(
+        token["token"] as string,
+        process.env.JWT_SECRET!
+        );
+        if (isTokenvalid.role === 'verifier') {
+        next();
+        } else {
+        res.status(401).send("Unauthorized User");
+        }
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+}
